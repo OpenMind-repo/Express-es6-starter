7ef3b588fcb9e6f0632694ceb911a5bb16f2bb7b
Gerald Halomoan Samosir <geralddeveloper95@gmail.com>
2017-08-29 12:56:10 +0700

    update

--- src/server/Module/index.js
+++ src/server/Module/index.js
@@ -1,6 +1,10 @@
-import  Blogmodule from  './Blogs/src/api'
-import  Ecomercemodule from  './Ecomerce/src/api'
+/**
+ * this is example to register module to folder module
+ */
+
+// import  Blogmodule from  './Blogs/src/api'
+// import  Ecomercemodule from  './Ecomerce/src/api'
 
 
 
-export  {Blogmodule ,Ecomercemodule}
+// export  {Blogmodule ,Ecomercemodule}
