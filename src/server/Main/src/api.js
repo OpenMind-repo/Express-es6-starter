6116b48b6a82d96ee3913b318a3513a255cc2c4c
Gerald Halomoan Samosir <geralddeveloper95@gmail.com>
2017-08-31 16:11:42 +0700

    update api

--- src/server/Main/src/api.js
+++ src/server/Main/src/api.js
@@ -10,13 +10,18 @@ const App =  Express()
 /**
  * registered module to Api
  */
-const { Blogmodule  ,Ecomercemodule } = require('_Module')
-
-App.use('/Blogs', Blogmodule)
-
-App.use('/Shops', Ecomercemodule)
 
 
+// const { Blogmodule  ,Ecomercemodule } = require('_Module')
+
+// App.use('/Blogs', Blogmodule)
+
+// App.use('/Shops', Ecomercemodule)
+
+App.use('/',(req,res)=>{
+    res.status(200)
+    res.json("welcome to api")
+})
 
 module.exports = App
 
